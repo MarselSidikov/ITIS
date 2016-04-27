ca0be20951991d42218fb12764e066a0176d49ae
MarselSidikov <sidikov.marsel@gmail.com>
2016-04-27 09:59:55 +0300

    fixed graph

--- 09-551/Graphs/main.cpp
+++ 09-551/Graphs/main.cpp
@@ -20,6 +20,7 @@ void Create(GraphMatrixImpl *&graph, int verticesCount)
 {
     if (verticesCount < MAX_VERTICES_COUNT)
     {
+        graph = new GraphMatrixImpl;
         graph->verticesCount = verticesCount;
         FillZeros(graph);
     } else cout << "Vertices to many" << endl;
@@ -41,15 +42,32 @@ void AddVertex(GraphMatrixImpl *&graph)
         graph->verticesCount++;
     } else cout << "Vertices to many" << endl;
 }
-
+void AddEdge(GraphMatrixImpl *&graph, int iVertex, int jVertex)
+{
+    if (iVertex < graph->verticesCount
+        && jVertex < graph->verticesCount)
+    {
+        graph->matrix[iVertex][jVertex] = 1;
+        graph->matrix[jVertex][iVertex] = 1;
+    } else cout << "Vertext not exist" << endl;
+}
 void Show(GraphMatrixImpl *graph)
 {
+    cout << "  ";
     for (int i = 0; i < graph->verticesCount; i++)
     {
+        cout << i << "|";
+    }
+    cout << endl;
+    int columnIndex = 0;
+    for (int i = 0; i < graph->verticesCount; i++)
+    {
+        cout << columnIndex << '|';
         for (int j = 0; j < graph->verticesCount; j++)
         {
             cout << graph->matrix[i][j] << ' ';
         }
+        columnIndex++;
         cout << endl;
     }
 }
@@ -57,5 +75,8 @@ int main()
 {
     GraphMatrixImpl *graph;
     Create(graph, 5);
+    AddEdge(graph, 2, 3);
+    AddEdge(graph, 4, 1);
     Show(graph);
+    AddEdge(graph,5,6);
 }
